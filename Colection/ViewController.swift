cc8120ea8f081ecee6be33e9223abb0d146bd595
Jennifer Ferreira <jennifersouzaferreira@outlook.com>
2021-05-30 20:57:02 -0300

    Estrutura

---
+++ Colection/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Colection
+//
+//  Created by Jennifer Ferreira on 30/05/21.
+//
+
+import UIKit
+ 
+// aula colection
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
